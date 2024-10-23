b912a53cef99d13a992d4a7f8fc9985be12d21fe
Mosqupa <137916950+Mosqupa@users.noreply.github.com>
2024-10-23 15:12:50 +0800

    Add files via upload

---
+++ Gauss.py
@@ -0,0 +1,40 @@
+import numpy as np
+import copy
+
+def gauss(A:np.array, x0:np.array, b:np.array) -> np.array:
+     x0 = x0.astype(np.float64) # 把x0数组的数变成float型
+     height = x0.shape[0] # 获取数组行数
+     print("iteration times = 0 , original vector : ", x0)
+     x1 = np.zeros(3)
+     for i in range(height): # i取0,1,2
+          sum = float(0)
+          for j in range(height): # sum获取除当前待计算的x之外的aij*xj之和
+               if j!=i:
+                    if j<i:
+                         sum += A[i][j] * x1[j]
+                    if j>i:
+                         sum += A[i][j] * x0[j]
+          x1[i] = (b[i]-sum)/A[i][i] # 完成更新
+     times = 1
+     print("iteration times =", times, ", result : ", x1)
+     while np.abs(np.max(np.abs(x0))-np.max(np.abs(x1))) > 10 ** -4:
+          times += 1
+          x0 = copy.deepcopy(x1)
+          for i in range(height): # i取0,1,2
+               sum = float(0)
+               for j in range(height): # sum获取除当前待计算的x之外的aij*xj之和
+                    if j!=i:
+                         if j<i:
+                              sum += A[i][j] * x1[j]
+                         if j>i:
+                              sum += A[i][j] * x0[j]
+               x1[i] = (b[i]-sum)/A[i][i] # 完成更新
+          print("iteration times =", times, ", result : ", x1)
+     return x0
+
+x0 = np.array([0,0,0])
+A = np.array([[ 5, 2, 1],
+              [-1, 4, 2],
+              [ 2,-3, 10]])
+b = np.array([-12,20,3])
+gauss(A, x0, b)
